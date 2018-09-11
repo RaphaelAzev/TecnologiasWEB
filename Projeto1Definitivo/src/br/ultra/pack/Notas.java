e85d277b25478a9a09297b597f75b16e3e12daee
RaphaelAzev <raphaelazev@hotmail.com>
2018-09-11 09:18:57 -0300

    Projeto 1 TecWeb

---
+++ Projeto1Definitivo/src/br/ultra/pack/Notas.java
@@ -0,0 +1,22 @@
+package br.ultra.pack;
+
+//import java.util.Calendar;
+
+public class Notas {
+	
+	private Integer id;
+	private String conteudo;
+	private String datacriacao;
+	
+	public Integer getId() {return this.id;}
+	public void setId(Integer id) {this.id = id;}
+	
+	public String getConteudo() {return this.conteudo;}
+	public void setConteudo(String conteudo) {this.conteudo = conteudo;}
+	
+	public String getDatacriacao() {return this.datacriacao;}
+	public void setDatacriacao(String datacriacao) {this.datacriacao = datacriacao;}
+	
+//	public Double getAltura() {return this.altura;}
+//	public void setAltura(Double altura) {this.altura = altura;}
+}
